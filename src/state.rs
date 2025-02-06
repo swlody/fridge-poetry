0748e3087da5cdff1754933083d429456afdadb9
Sam Wlody <swlody@gmail.com>
2025-02-05 21:50:42 -0500

    do some config stuff

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,10 @@
+use std::sync::Arc;
+
+use crate::config::Config;
+
 #[derive(Clone, Debug)]
 pub struct AppState {
     pub postgres: sqlx::PgPool,
     pub magnet_updates: tokio::sync::broadcast::Sender<String>,
+    pub config: Arc<Config>,
 }
