663b0244c082c6361d73d5411f6d6ab589acfb9c
Sam Wlody <swlody@gmail.com>
2025-02-15 00:37:20 -0500

    seed stuff

---
+++ src/bin/generate_table.rs
@@ -0,0 +1,44 @@
+use std::{
+    fs::File,
+    io::{BufRead as _, BufReader},
+};
+
+use rand::{seq::IndexedRandom as _, Rng as _, SeedableRng};
+
+const NUM_MAGNETS: i32 = 20_000_000;
+const MAX_X: i32 = 500_000;
+const ROTATION_RATIO: f64 = 1.0 / 50.0;
+const REGULAR_ROTATION: i32 = 5;
+const TWEAKED_ROTATION: i32 = 50;
+
+fn main() {
+    let f = File::open("seeds/word_list.txt").unwrap();
+    let reader = BufReader::new(f);
+    let words: Vec<String> = reader
+        .lines()
+        .map(Result::unwrap)
+        .filter(|line| !line.starts_with("/"))
+        .collect();
+
+    let mut rng = rand::rngs::SmallRng::from_os_rng();
+    let mut writer = csv::Writer::from_path("seeds/magnets.csv").unwrap();
+    for _ in 0..NUM_MAGNETS {
+        let word = words.choose(&mut rng).unwrap();
+        let x = rng.random_range(-MAX_X..=MAX_X);
+        let y = rng.random_range(-MAX_X..=MAX_X);
+
+        let rotation = if rng.random_bool(ROTATION_RATIO) {
+            rng.random_range(TWEAKED_ROTATION..=(360 - TWEAKED_ROTATION))
+        } else {
+            rng.random_range(-REGULAR_ROTATION..=REGULAR_ROTATION)
+        };
+
+        writer
+            .write_record(&[
+                format!("({},{})", x, y).parse().unwrap(),
+                rotation.to_string(),
+                word.to_string(),
+            ])
+            .unwrap();
+    }
+}
